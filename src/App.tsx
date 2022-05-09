b98a7117bee98cb5eb26d4af5d3812393ccb61bf
Kaique Borges <rique.kaique@gmail.com>
2022-05-09 19:38:34 -0300

    Add ids and testing script from FreeCodeCamp

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,67 @@ function App() {
 
   return (
     <div className="calculator">
-      <div className="display">670+6887</div>
+      <div id="display" className="display">
+        670+6887
+      </div>
       <div className="numpad">
-        <button className="numkey numkey--dimmed">C</button>
+        <button id="clear" className="numkey numkey--dimmed">
+          C
+        </button>
         <button className="numkey numkey--dimmed">+/-</button>
         <button className="numkey numkey--dimmed">%</button>
-        <button className="numkey numkey--highlighted">/</button>
+        <button id="divide" className="numkey numkey--highlighted">
+          /
+        </button>
 
-        <button className="numkey">7</button>
-        <button className="numkey">8</button>
-        <button className="numkey">9</button>
-        <button className="numkey numkey--highlighted">x</button>
+        <button id="seven" className="numkey">
+          7
+        </button>
+        <button id="eight" className="numkey">
+          8
+        </button>
+        <button id="nine" className="numkey">
+          9
+        </button>
+        <button id="multiply" className="numkey numkey--highlighted">
+          x
+        </button>
 
-        <button className="numkey">4</button>
-        <button className="numkey">5</button>
-        <button className="numkey">6</button>
-        <button className="numkey numkey--highlighted">-</button>
+        <button id="four" className="numkey">
+          4
+        </button>
+        <button id="five" className="numkey">
+          5
+        </button>
+        <button id="six" className="numkey">
+          6
+        </button>
+        <button id="subtract" className="numkey numkey--highlighted">
+          -
+        </button>
 
-        <button className="numkey">1</button>
-        <button className="numkey">2</button>
-        <button className="numkey">3</button>
-        <button className="numkey numkey--highlighted">+</button>
+        <button id="one" className="numkey">
+          1
+        </button>
+        <button id="two" className="numkey">
+          2
+        </button>
+        <button id="three" className="numkey">
+          3
+        </button>
+        <button id="add" className="numkey numkey--highlighted">
+          +
+        </button>
 
-        <button className="numkey colspan-2">0</button>
-        <button className="numkey">,</button>
-        <button className="numkey numkey--highlighted">=</button>
+        <button id="zero" className="numkey colspan-2">
+          0
+        </button>
+        <button id="decimal" className="numkey">
+          .
+        </button>
+        <button id="equals" className="numkey numkey--highlighted">
+          =
+        </button>
       </div>
     </div>
   )
