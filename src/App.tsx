11018ebd8529a31251f501d184481d189fb580d1
Kaique Borges <rique.kaique@gmail.com>
2022-05-10 22:08:39 -0300

    Add dummy click handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,156 @@
 import { useState } from 'react'
 import './App.css'
 
+type Operation = 'add' | 'subtract' | 'multiply' | 'divide'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [expression, setExpression] = useState('')
+  const [display, setDisplay] = useState('0')
+
+  const clearClickHandler = () => {
+    console.log('clear')
+  }
+
+  const operationClickHandler = (operation: Operation) => {
+    console.log(operation)
+  }
+
+  const numberClickHandler = (numStr: string) => {
+    console.log(numStr)
+  }
+
+  const decimalClickHandler = () => {
+    console.log('decimal')
+  }
+
+  const equalsClickHandler = () => {
+    console.log('equals')
+  }
 
   return (
     <div className="calculator">
-      <div id="display" className="display">
-        670+6887
+      <div className="display--container">
+        <div id="expression">{expression}</div>
+        <div id="display">{display}</div>
       </div>
       <div className="numpad">
-        <button id="clear" className="numkey numkey--dimmed">
-          C
+        <button
+          onClick={clearClickHandler}
+          id="clear"
+          className="numkey numkey--dimmed colspan-3"
+        >
+          AC
         </button>
-        <button className="numkey numkey--dimmed">+/-</button>
-        <button className="numkey numkey--dimmed">%</button>
-        <button id="divide" className="numkey numkey--highlighted">
+        <button
+          onClick={() => operationClickHandler('divide')}
+          id="divide"
+          className="numkey numkey--highlighted"
+        >
           /
         </button>
 
-        <button id="seven" className="numkey">
+        <button
+          onClick={() => numberClickHandler('7')}
+          id="seven"
+          className="numkey"
+        >
           7
         </button>
-        <button id="eight" className="numkey">
+        <button
+          onClick={() => numberClickHandler('8')}
+          id="eight"
+          className="numkey"
+        >
           8
         </button>
-        <button id="nine" className="numkey">
+        <button
+          onClick={() => numberClickHandler('9')}
+          id="nine"
+          className="numkey"
+        >
           9
         </button>
-        <button id="multiply" className="numkey numkey--highlighted">
+        <button
+          onClick={() => operationClickHandler('multiply')}
+          id="multiply"
+          className="numkey numkey--highlighted"
+        >
           x
         </button>
 
-        <button id="four" className="numkey">
+        <button
+          onClick={() => numberClickHandler('4')}
+          id="four"
+          className="numkey"
+        >
           4
         </button>
-        <button id="five" className="numkey">
+        <button
+          onClick={() => numberClickHandler('5')}
+          id="five"
+          className="numkey"
+        >
           5
         </button>
-        <button id="six" className="numkey">
+        <button
+          onClick={() => numberClickHandler('6')}
+          id="six"
+          className="numkey"
+        >
           6
         </button>
-        <button id="subtract" className="numkey numkey--highlighted">
+        <button
+          onClick={() => operationClickHandler('subtract')}
+          id="subtract"
+          className="numkey numkey--highlighted"
+        >
           -
         </button>
 
-        <button id="one" className="numkey">
+        <button
+          onClick={() => numberClickHandler('1')}
+          id="one"
+          className="numkey"
+        >
           1
         </button>
-        <button id="two" className="numkey">
+        <button
+          onClick={() => numberClickHandler('2')}
+          id="two"
+          className="numkey"
+        >
           2
         </button>
-        <button id="three" className="numkey">
+        <button
+          onClick={() => numberClickHandler('3')}
+          id="three"
+          className="numkey"
+        >
           3
         </button>
-        <button id="add" className="numkey numkey--highlighted">
+        <button
+          onClick={() => operationClickHandler('add')}
+          id="add"
+          className="numkey numkey--highlighted"
+        >
           +
         </button>
 
-        <button id="zero" className="numkey colspan-2">
+        <button
+          onClick={() => numberClickHandler('0')}
+          id="zero"
+          className="numkey colspan-2"
+        >
           0
         </button>
-        <button id="decimal" className="numkey">
+        <button onClick={decimalClickHandler} id="decimal" className="numkey">
           .
         </button>
-        <button id="equals" className="numkey numkey--highlighted">
+        <button
+          onClick={equalsClickHandler}
+          id="equals"
+          className="numkey numkey--highlighted"
+        >
           =
         </button>
       </div>
